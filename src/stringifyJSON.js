5fc01e5d0e810ea3065705124506a0c02726f8e5
Mike <mbarton.mail@gmail.com>
2014-04-23 23:03:34 -0400

    Reimplemented Stringify for primitive stuff and Arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,28 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // return null for functions and undefined things
+  // here are what seems to be the basics
+ 	 if (typeof(obj) === 'number' || obj === true || obj === false){
+ 	 	return obj.toString();
+ 	 }
+ 	 else if (typeof(obj) === 'string')
+  		return "\"" + obj + "\"";
+	 else if (Object.prototype.toString.call(obj) == '[object Function]')
+ 	 	return null;
+ 	 else if (obj === null)
+  		return 'null'
+
+  // Arrays
+  else if (Object.prototype.toString.call(obj) == '[object Array]'){
+  	var arr = []
+  	for (var i = 0; i < obj.length; i++)
+  		arr.push(stringifyJSON(obj[i]))
+  	return "[" + arr.join(",") + "]";
+  }
+  // Objects
+  else if (typeof(obj) === 'object')
+  	return "{" + "}"
+
+  return 'wut'
 };
