c1c9f401440a031e2ad439543fdb2048ff72178e
Mike <mbarton.mail@gmail.com>
2014-04-25 23:29:47 -0400

    Cleaned code.  All REQUIRED precourse work is complete~

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,47 +5,29 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // this code should use document.body, element.childNodes, and element.classList
-  // x.hasChildNodes
   // note to self: document.body gave weird results when used in a different environment
-
-  $('body').addClass('targetClassName');
-  $('body').append('<p><p class="targetClassName"></p></p>')
   var nodeList = [];
-  var children = document.body.childNodes
-
-  console.log("document.body: " + document.body.childNodes)
-  console.log("document.body.childNodes: " + document.body.childNodes);
-  console.log("document.body.classList: " + document.body.classList);
-  console.log("nodeList: " + nodeList)
-
-  //console.log(children[3].classList)
-
-  findElements(document.body)
 
   function hasTargetClass(data){
-  	reggy = new RegExp(className);
-  	return reggy.test(data)
+  	return data === className
   }
 
   function findElements(node){
-  	console.log(node.classList)
+  	// Push nodes with className to nodeList
   	_.each(node.classList, function(element){
-  		console.log(2)
   		if (hasTargetClass(element))
   			nodeList.push(node)
  	})
-
+  	// Pass all children to findElements
   	if (node.childNodes){
-  		console.log(1)
   		_.each(node.childNodes, function(element){
   			findElements(element)
   		});
   	}
-  	 	
   }
 
-  console.log(nodeList)
+  findElements(document.body)
+
   return nodeList;
 
 };
